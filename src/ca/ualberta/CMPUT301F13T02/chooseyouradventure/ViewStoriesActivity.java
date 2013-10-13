7e977ec2c5207b75d3a4fbdbbc6ee18b06028957
jcadek <jcadek@ualberta.ca>
2013-10-13 14:27:45 -0600

    More UI additions

--- src/ca/ualberta/CMPUT301F13T02/chooseyouradventure/ViewStoriesActivity.java
+++ src/ca/ualberta/CMPUT301F13T02/chooseyouradventure/ViewStoriesActivity.java
@@ -6,11 +6,20 @@ import java.util.ArrayList;
 
 
 
+
+
+
+
+
 import android.os.Bundle;
+
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -19,14 +28,14 @@ import android.widget.ListView;
  * @uml.dependency   supplier="ca.ualberta.CMPUT301F13T02.chooseyouradventure.ViewPageActivity"
  */
 public class ViewStoriesActivity extends Activity {
-	private ListView lv;
+	private ListView mainPage;
 	private String[] listText;
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.view_stories_activity);
-        lv = (ListView) findViewById(R.id.mainView);
+        mainPage = (ListView) findViewById(R.id.mainView);
         
     }
 
@@ -54,7 +63,25 @@ public class ViewStoriesActivity extends Activity {
 		listText = newList.toArray(new String[newList.size()]);
 		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
 				R.layout.list_item_base, listText);
-		lv.setAdapter(adapter);
+		mainPage.setAdapter(adapter);
+		
+		
+		//based on http://android.konreu.com/developer-how-to/click-long-press-event-listeners-list-activity/
+		mainPage.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+		    @Override
+		    public void onItemClick(AdapterView<?> av, View v, int pos, long listNum) {
+		        onListItemClick(v,pos,listNum);
+		    }
+		});
+
+		mainPage.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+		    @Override
+		    public boolean onItemLongClick(AdapterView<?> av, View v, int pos, long listNum) {
+		        return onLongListItemClick(v,pos,listNum);
+		    }
+		});
+
+
 		
 		
 	}
@@ -68,7 +95,53 @@ public class ViewStoriesActivity extends Activity {
 		Intent intent = new Intent(this, ViewPageActivity.class);
 		startActivity(intent);
 	}
+    
+    protected void onListItemClick(View v, int pos, long id) {
+	    jumpPage(v);
+	}
+    
+    
 
+    
+    
+   
+	public void storyMenu(final View v){
+			final String[] titles = {"Edit","Upload","Cache","Delete","Cancel"};
+			
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle(R.string.story_options);
+            builder.setItems(titles, new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int item) {
+                	switch(item){
+                	case(0):
+                		jumpEdit(v);
+                		break;
+                	case(1):
+                		
+                		break;
+                	case(2):
+                		
+                		break;
+                	case(3):
+                		
+                		break;
+                	}
+                        
+                    }});
+            builder.show();
+        }
+
+
+    protected boolean onLongListItemClick(View v, int pos, long id) { 
+    	storyMenu(v);
+        return true;
+    }
+
+
+	
+
+
+	
 
 
     

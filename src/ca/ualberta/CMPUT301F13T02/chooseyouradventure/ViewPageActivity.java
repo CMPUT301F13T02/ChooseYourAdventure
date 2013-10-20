4107cd5add11c505a6bfdae39fd461e961a8eb8d
Konrad Lindenbach <konrad.lindenbach@gmail.com>
2013-10-20 12:25:35 -0600

    fixed ViewPageActivity... sorry about that

--- src/ca/ualberta/CMPUT301F13T02/chooseyouradventure/ViewPageActivity.java
+++ src/ca/ualberta/CMPUT301F13T02/chooseyouradventure/ViewPageActivity.java
@@ -19,11 +19,13 @@ public class ViewPageActivity extends Activity {
 		startActivity(intent);
 	}
 	
+	/*
 	@Override
 	public void onResume() {
 		//MyApplication app = (MyApplication) getApplication();
 		//displayPage(app);
 	}
+	*/
 	
 	//private void displayPage(MyApplication app) {
 		//Page page = app.getPage();

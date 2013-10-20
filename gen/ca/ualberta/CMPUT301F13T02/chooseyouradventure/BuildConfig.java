69ef4d0a17d7a8d4707bf24978f3e6f72495209b
moorej12 <moorejames099@gmail.com>
2013-10-19 23:02:22 -0600

    adding gen files so I can pull changes

---
+++ gen/ca/ualberta/CMPUT301F13T02/chooseyouradventure/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package ca.ualberta.CMPUT301F13T02.chooseyouradventure;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}

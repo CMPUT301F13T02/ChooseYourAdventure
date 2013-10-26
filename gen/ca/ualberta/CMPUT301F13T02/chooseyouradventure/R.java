08c8c120793c7dfedcdf55cd0f87cd5011d7e168
301git <jcadek@ualberta.ca>
2013-10-26 16:10:36 -0600

    dds story tree and fixes bugs

--- gen/ca/ualberta/CMPUT301F13T02/chooseyouradventure/R.java
+++ gen/ca/ualberta/CMPUT301F13T02/chooseyouradventure/R.java
@@ -24,19 +24,22 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int TextTiletextView=0x7f080009;
-        public static final int action_settings=0x7f08000c;
-        public static final int addComment=0x7f080008;
-        public static final int button1=0x7f080002;
-        public static final int button2=0x7f080003;
-        public static final int commentTitle=0x7f080006;
+        public static final int TextTiletextView=0x7f08000a;
+        public static final int action_settings=0x7f08000f;
+        public static final int addComment=0x7f080009;
+        public static final int button1=0x7f08000b;
+        public static final int button2=0x7f08000e;
+        public static final int commentTitle=0x7f080007;
         public static final int commentsTextView=0x7f080000;
-        public static final int commentsView=0x7f080007;
-        public static final int createButton=0x7f08000a;
+        public static final int commentsView=0x7f080008;
+        public static final int createButton=0x7f08000c;
+        public static final int createButton2=0x7f080003;
         public static final int decisionTextView=0x7f080001;
-        public static final int decisionsView=0x7f080005;
-        public static final int mainView=0x7f08000b;
-        public static final int tilesView=0x7f080004;
+        public static final int decisionsView=0x7f080006;
+        public static final int deleteButton=0x7f080002;
+        public static final int mainView=0x7f08000d;
+        public static final int tilesView=0x7f080005;
+        public static final int treeView=0x7f080004;
     }
     public static final class layout {
         public static final int comments_adapter_layout=0x7f030000;
@@ -53,22 +56,23 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int add=0x7f050007;
-        public static final int add_comment_button=0x7f050011;
-        public static final int add_new=0x7f05000f;
+        public static final int add_comment_button=0x7f050012;
+        public static final int add_new=0x7f050010;
         public static final int app_name=0x7f050000;
-        public static final int cache=0x7f05000a;
-        public static final int cancel=0x7f05000c;
-        public static final int comment_title=0x7f050010;
+        public static final int cache=0x7f05000b;
+        public static final int cancel=0x7f05000d;
+        public static final int comment_title=0x7f050011;
         public static final int delete=0x7f050006;
-        public static final int edit=0x7f050009;
-        public static final int hello_world=0x7f05000d;
+        public static final int edit=0x7f05000a;
+        public static final int hello_world=0x7f05000e;
+        public static final int page_options=0x7f050009;
         public static final int search=0x7f050005;
-        public static final int search_stories=0x7f05000e;
+        public static final int search_stories=0x7f05000f;
         public static final int story_options=0x7f050008;
         public static final int title_edit_story_activity=0x7f050004;
         public static final int title_view_page_activity=0x7f050002;
         public static final int title_view_stories_activity=0x7f050003;
-        public static final int upload=0x7f05000b;
+        public static final int upload=0x7f05000c;
     }
     public static final class style {
         /** 

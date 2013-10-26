862fe564887489a2fc86a0c60f162625311879f0
moorej12 <moorejames099@gmail.com>
2013-10-26 15:37:23 -0600

    Switched to storing info in Application

--- gen/ca/ualberta/CMPUT301F13T02/chooseyouradventure/R.java
+++ gen/ca/ualberta/CMPUT301F13T02/chooseyouradventure/R.java
@@ -24,16 +24,18 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int TextTiletextView=0x7f080007;
-        public static final int action_settings=0x7f08000a;
+        public static final int TextTiletextView=0x7f080009;
+        public static final int action_settings=0x7f08000c;
+        public static final int addComment=0x7f080008;
         public static final int button1=0x7f080002;
         public static final int button2=0x7f080003;
+        public static final int commentTitle=0x7f080006;
         public static final int commentsTextView=0x7f080000;
-        public static final int commentsView=0x7f080006;
-        public static final int createButton=0x7f080008;
+        public static final int commentsView=0x7f080007;
+        public static final int createButton=0x7f08000a;
         public static final int decisionTextView=0x7f080001;
         public static final int decisionsView=0x7f080005;
-        public static final int mainView=0x7f080009;
+        public static final int mainView=0x7f08000b;
         public static final int tilesView=0x7f080004;
     }
     public static final class layout {
@@ -51,10 +53,12 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int add=0x7f050007;
+        public static final int add_comment_button=0x7f050011;
         public static final int add_new=0x7f05000f;
         public static final int app_name=0x7f050000;
         public static final int cache=0x7f05000a;
         public static final int cancel=0x7f05000c;
+        public static final int comment_title=0x7f050010;
         public static final int delete=0x7f050006;
         public static final int edit=0x7f050009;
         public static final int hello_world=0x7f05000d;

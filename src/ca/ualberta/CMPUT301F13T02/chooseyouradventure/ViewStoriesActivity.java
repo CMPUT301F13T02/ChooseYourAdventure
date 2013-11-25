f1419557d362a5c3bb7a765cf09cb532be0275b2
Konrad Lindenbach <konrad.lindenbach@gmail.com>
2013-11-24 18:22:13 -0700

    #226 Moved all buttons except add new to action bar

--- src/ca/ualberta/CMPUT301F13T02/chooseyouradventure/ViewStoriesActivity.java
+++ src/ca/ualberta/CMPUT301F13T02/chooseyouradventure/ViewStoriesActivity.java
@@ -36,10 +36,6 @@ import java.util.ArrayList;
 
 import android.app.Activity;
 import android.app.AlertDialog;
-import android.app.SearchManager;
-import android.app.SearchableInfo;
-import android.content.ComponentName;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.provider.Settings.Secure;
@@ -54,9 +50,6 @@ import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ListView;
-import android.widget.ScrollView;
-import android.widget.SearchView;
-
 import android.widget.TextView;
 import ca.ualberta.CMPUT301F13T02.chooseyouradventure.elasticsearch.ESHandler;
 
@@ -81,18 +74,15 @@ import ca.ualberta.CMPUT301F13T02.chooseyouradventure.elasticsearch.ESHandler;
  */
 
 public class ViewStoriesActivity extends Activity {
+
 	private ListView mainPage;
 	private Button createNew;
-	private Button searchButton;
-	private Button randomStoryButton;
-	private Button refreshButton;
 	ArrayList<String> storyText = new ArrayList<String>();
 	ArrayList<Story> storyList = new ArrayList<Story>();
 	private ControllerApp app; 
 	private SampleGenerator sampleGen = new SampleGenerator();
 	private Handler eshandler = new ESHandler();
 	private Handler dbhandler = new DBHandler(this);
-	private static final int HELP_INDEX = 0;
 	
 	ArrayAdapter<String> adapter;
     @Override
@@ -101,38 +91,10 @@ public class ViewStoriesActivity extends Activity {
         setContentView(R.layout.view_stories_activity);
         mainPage = (ListView) findViewById(R.id.mainView);
         createNew = (Button) findViewById(R.id.createButton);
-        searchButton = (Button) findViewById(R.id.searchButton);
-        randomStoryButton = (Button) findViewById(R.id.randomButton);
-        refreshButton = (Button) findViewById(R.id.button1);
         createNew.setOnClickListener(new OnClickListener() {
             public void onClick(View v) {
                 createStory();
             }
-        });
-        refreshButton.setOnClickListener(new OnClickListener() {
-            
-            public void onClick(View v) {
-                refresh();
-            }
-        });
-        
-        searchButton.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				onSearchRequested();
-			}
-        	
-        });
-        
-        randomStoryButton.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				onRandomStory();
-				
-			}
-        	
         });
         
         app = (ControllerApp) getApplication();
@@ -175,15 +137,13 @@ public class ViewStoriesActivity extends Activity {
         
     }
     
-    protected void onRandomStory() {
+    public void onRandomStory() {
 		try {
 			Story random = eshandler.getRandomStory();
 			app.jump(ViewPageActivity.class, random, random.getFirstpage());
 		} catch (HandlerException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
 	}
 
 	@Override
@@ -192,8 +152,6 @@ public class ViewStoriesActivity extends Activity {
         refresh();
     }
         
-
-
     /**
      * Inflate the options menu; this adds items to the action bar if it is present 
      * 
@@ -205,9 +163,6 @@ public class ViewStoriesActivity extends Activity {
     	super.onCreateOptionsMenu(menu);
     	
     	getMenuInflater().inflate(R.menu.view_stories, menu);
-
-		MenuItem help = menu.add(0, HELP_INDEX, HELP_INDEX, getString(R.string.help));
-		help.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
 
         return true;
     }
@@ -222,11 +177,20 @@ public class ViewStoriesActivity extends Activity {
     public boolean onOptionsItemSelected(MenuItem item) 
     {
 		switch (item.getItemId()) {
-		case HELP_INDEX:
-
-			AlertDialog dialog = HelpDialogFactory.create(R.string.view_stories_help, this);
-			dialog.show();
+		case R.id.action_help:
+			HelpDialogFactory.create(R.string.view_stories_help, this).show();
+			break;
 	        
+		case R.id.action_refresh:
+            refresh();
+            break;
+       
+		case R.id.action_search:
+			onSearchRequested();
+		    break;	
+		        
+		case R.id.action_randomStory:
+			onRandomStory();
 			break;
 		}
 		return true;
